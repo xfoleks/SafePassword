552c7f9cf27f338e4aec879997d1d2f3307dd1bc
Oleksandr Filonenko <filonenko.oleksa@gmail.com>
2023-07-05 17:35:50 +0300

    Implement splash screen of the app

---
+++ app/src/main/java/edu/xfoleks/safepassword/presentation/activities/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package edu.xfoleks.safepassword.presentation.activities.splashscreen
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import edu.xfoleks.safepassword.presentation.activities.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        } else {
+            actionBar?.hide()
+            Handler().postDelayed({
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
+    }
+}
